fadf54aa30d59c60b0d649088bc49b0b47507139
unknown <gamba142@gmail.com>
2019-06-27 18:43:03 -0300

    Solução final aula 4 - Adicionando novos todos

--- MOD3/EX4/todos.js
+++ MOD3/EX4/todos.js
@@ -9,6 +9,7 @@ var todos = [
 ];
 
 function renderTodos() {
+    listElement.innerHTML = '';
     for (todo of todos){
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
@@ -21,5 +22,11 @@ function renderTodos() {
 renderTodos();
 
 function addTodo(){
-    
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
 }
+
+buttonElement.onclick = addTodo;
